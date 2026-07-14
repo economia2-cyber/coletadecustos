2c60a543b416d741f13f5a700f647d57350b426b
economia2-cyber <economia2@aprosojams.org.br>
2026-07-14 10:55:40 -0400

    PWA Precos: comparacao com a media da regiao (pwa-precos/sw.js)

--- pwa-precos/sw.js
+++ pwa-precos/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aprosoja-precos-v26';
+const CACHE = 'aprosoja-precos-v27';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './Aprosoja-logo.png', './qr-app.png'];
 
 self.addEventListener('install', e => {
